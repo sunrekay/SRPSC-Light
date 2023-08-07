33e2e06c925ee2663623b939aaad3b20ceb94235
sunrekay <73135012+sunrekay@users.noreply.github.com>
2023-08-07 03:53:07 -0700

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from mysql.connector import connect, Error
 from settings import DB_AUTH
 
 
-def get_all_proxy():
+def create_proxy_counter_row(ip: str, geo: str):
     try:
         with connect(
                 host=DB_AUTH["host"],
@@ -10,15 +10,19 @@ def get_all_proxy():
                 password=DB_AUTH["password"]
         ) as connection:
             with connection.cursor() as cursor:
-                cursor.execute("USE google_bots_monitoring;")
-                cursor.execute(f"SELECT * FROM proxy;")
-                result = cursor.fetchall()
-        return result
+                cursor.execute("USE proxy_shop;")
+                cursor.execute(f"INSERT INTO proxy_counter VALUES "
+                               f"('{ip}',"
+                               f" '{geo}',"
+                               f" 0"
+                               ");")
+                connection.commit()
+        return True
     except Error as e:
         print(e)
-        return ()
+        return False
 
-
+    
 def send_proxy(ip: str, port: str, login: str, password: str, status: str = "yes"):
     try:
         with connect(
@@ -27,13 +31,8 @@ def send_proxy(ip: str, port: str, login: str, password: str, status: str = "yes
                 password=DB_AUTH["password"]
         ) as connection:
             with connection.cursor() as cursor:
-                new_proxy: tuple = (ip, port, login, password, status)
-                db_proxies = get_all_proxy()
 
-                if new_proxy in db_proxies:
-                    return True
-
-                cursor.execute("USE google_bots_monitoring;")
+                cursor.execute("USE proxy_shop;")
                 cursor.execute(f"INSERT INTO proxy VALUES "
                                f"('{ip}',"
                                f" '{port}',"
